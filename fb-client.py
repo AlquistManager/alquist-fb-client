62009a96764f32c826c6b2f351c7395f9e98181a
Jakub Konrád <konrajak@fel.cvut.cz>
2016-09-05 11:23:43 +0200

    Initial commit

---
+++ fb-client.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import json
+import datetime
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+sessions = {}
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must
+    # return the 'hub.challenge' value in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    session_id = find_session(sender_id)
+                    send_to_alquist(session_id, message_text, sessions[session_id]['context'], sessions[session_id]['state'])
+
+                    send_message(sender_id, "got it, thanks!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+def find_session(user_id):
+    session_id = -1
+    for session in sessions:
+        if sessions[session].get('uid', False) == user_id:
+            session_id = session
+
+    if session_id == -1:
+        session_id = datetime.datetime.utcnow().isoformat()
+        sessions[session_id] = {'uid': user_id, 'context': {}, 'state': 'init'}
+
+    return session_id
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+def send_to_alquist(session_id, message_text, context, state):
+    log("sending message to Alquist: {session}, {text}".format(session=session_id, text=message_text))
+
+    params = {
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "session": session_id,
+        "text": message_text,
+        "context": context,
+        "state": state
+
+    })
+    r = requests.post("https://alquist.herokuapp.com/", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print(str(message))
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
